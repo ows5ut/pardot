06f0b19e5f1be7fe7e3810f45a2e6b0e6c33da4d
ows5ut <ows5ut@gmail.com>
2021-07-27 12:58:19 +0300

    update

--- api/api.js
+++ api/api.js
@@ -30,12 +30,3 @@ let API = {
 }
 
 module.exports = API;
-
-
-// curl --location --request POST 'https://mart.my.salesforce.com/services/oauth2/token' \
-// --header 'Content-Type: application/x-www-form-urlencoded' \
-// --data-urlencode 'grant_type=password' \
-// --data-urlencode 'username=ows5ut-aj03@force.com' \
-// --data-urlencode 'password=12348765V2CgneDfsZYuNxx29nn0fL3tF' \
-// --data-urlencode 'client_id=3MVG9SOw8KERNN09cesmLO4.5XwgchJyMl1d2W.CfMd_DBiJhG3UYyZcjjU.iM1jQf2KWoIzX9WxcJ3c70nPx' \
-// --data-urlencode 'client_secret=3D281686ED1186E2C6FAE506611654E8E8627DA089CFCB105226DE5E8FCBD08A'
